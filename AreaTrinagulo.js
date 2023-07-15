427474cfd6d649f0735b88e2377834786c987027
FilippiMnz <imperilkil12@gmail.com>
2023-07-14 21:29:21 -0300

    Kono Dio Da

---
+++ AreaTrinagulo.js
@@ -0,0 +1,5 @@
+    const AreaTriangulo =(a,b) => a*b/2
+    const Calculo = AreaTriangulo(2,7);
+    console.log (Calculo)
+
+    
---
+++ CircuferenciaCirculo.js
@@ -0,0 +1,3 @@
+const CircuferenciaCirculo = (raio, pi = 3.14) => 2*pi*raio
+    const CalcCircuferencia =CircuferenciaCirculo(6);
+    console.log(CalcCircuferencia)
